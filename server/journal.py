ccf14b4a57b3a64794fc909e3f4bac229ac799c6
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2013-10-20 12:37:44 -0400

    Got year and week number archives to work

--- server/journal.py
+++ server/journal.py
@@ -39,16 +39,65 @@ class Journal:
 			posts.append(Post(row))
 		cur.close()
 		t = self.env.get_template('index.html')
-		return t.render(posts=posts)
+		return t.render(title="Recent Posts",posts=posts)
 
 	@cherrypy.expose
 	def post(self, slug_text):
 		cur = self._get_cursor()
-		cur.execute('SELECT * FROM posts WHERE slug = ? LIMIT 1', slug_text)
+		cur.execute('SELECT * FROM posts WHERE slug = ? LIMIT 1', (slug_text,))
 		p = Post(cur.fetchone())
 		cur.close()
 		t = self.env.get_template('view_post.html')
 		return t.render(post=p)
+
+	@cherrypy.expose
+	def posts(self, *args):
+		if len(args) < 1:
+			return "TODO: calendar"
+
+		if len(args) > 1:
+			if args[1] == 'week':
+				return self._posts_from_year_and_week(*args)
+
+		# Posts within the entire year
+		cur = self._get_cursor()
+		cur.execute('SELECT * FROM posts WHERE STRFTIME("%Y",posted) = ? ORDER BY posted', (args[0],))
+		posts = []
+		for row in cur.fetchall():
+			posts.append(Post(row))
+		cur.close()
+
+		t = self.env.get_template('index.html')
+
+		if len(posts) == 1:
+			count = "1 post"
+		else:
+			count = "%d posts" % (len(posts),)
+
+		return t.render(title="%s in %s" % (count, args[0]),posts=posts)
+
+	def _posts_from_year_and_week(self, *args):
+		year = args[0]
+		month = args[2]
+
+		# Posts within year and same week
+		cur = self._get_cursor()
+		cur.execute('SELECT * FROM posts WHERE STRFTIME("%Y",posted) = ? AND STRFTIME("%W",posted) = ? ORDER BY posted', (year, month))
+		posts = []
+		for row in cur.fetchall():
+			posts.append(Post(row))
+		cur.close()
+
+		t = self.env.get_template('index.html')
+		d1 = posts[0].posted
+		d2 = posts[-1].posted
+
+		if len(posts) == 1:
+			count = "1 post"
+		else:
+			count = "%d posts" % (len(posts),)
+
+		return t.render(title="%s between %s and %s" % (count, d1.strftime("%x"), d2.strftime("%x")), posts=posts)
 
 if __name__ == '__main__':
 	cherrypy.quickstart(Journal())
