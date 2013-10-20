9291d63b0ef32fce18f43463c08b398e698e3767
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2013-10-20 09:59:01 -0400

    Moved from private git repo

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name VARCHAR,
+	email VARCHAR,
+	password VARCHAR
+);
+
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	author_id INTEGER REFERENCES users (id),
+	posted TIMESTAMP,
+	body TEXT
+);
+
+INSERT INTO users (name,email,password) VALUES
+	("Kiel","kiel.lofstrand@gmail.com","password");
+INSERT INTO users (name,email,password) VALUES
+	("Brittany","brit0000@hotmail.com","password");
+
+INSERT INTO posts (title,author_id,posted,body) VALUES
+	("My First Post",1,DATETIME("now"),
+"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
---
+++ server/journal.py
@@ -0,0 +1,52 @@
+import sqlite3
+import cherrypy
+import jinja2
+import markdown
+import datetime
+
+def connect(thread_id):
+	# Store a connection to our database in each thread
+	cherrypy.thread_data.db = sqlite3.connect('db/test.db')
+
+# Tell CherryPy to call connect for each thread
+cherrypy.engine.subscribe('start_thread', connect)
+
+def filter_markdown(text):
+	return markdown.markdown(text)
+
+class Post:
+	def __init__(self, record=[]):
+		self._id = record[0]
+		self.title = record[1]
+		self.author_id = record[2]
+		self.posted = record[3]
+		self.body = record[4]
+
+class Journal:
+	env = jinja2.Environment(loader=jinja2.FileSystemLoader('html'))
+	env.filters['markdown'] = filter_markdown
+
+	def _get_cursor(self):
+		return cherrypy.thread_data.db.cursor()
+
+	@cherrypy.expose
+	def index(self):
+		cur = self._get_cursor()
+		cur.execute('SELECT * FROM posts ORDER BY posted DESC')
+		posts = []
+		for row in cur.fetchall():
+			posts.append(Post(row))
+		cur.close()
+		t = self.env.get_template('index.html')
+		return t.render(posts=posts)
+
+	@cherrypy.expose
+	def test(self):
+		cur = self._get_cursor()
+		cur.execute('SELECT * FROM posts ORDER BY posted DESC')
+		result = cur.fetchone()
+		cur.close()
+		return str(result)
+
+if __name__ == '__main__':
+	cherrypy.quickstart(Journal())
