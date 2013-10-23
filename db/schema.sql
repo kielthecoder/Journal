5a3724f3cb5e43f7c26debf6b369c9f0c695833f
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2013-10-23 09:15:09 -0400

    Added some tags to the schema

--- db/schema.sql
+++ db/schema.sql
@@ -8,12 +8,24 @@ CREATE TABLE users (
 
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
-	id INTEGER PRIMARY KEY,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	title TEXT,
 	author_id INTEGER REFERENCES users (id),
 	posted TIMESTAMP,
 	body TEXT,
 	slug TEXT
+);
+
+DROP TABLE IF EXISTS tags;
+CREATE TABLE tags (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name VARCHAR
+);
+
+DROP TABLE IF EXISTS tags_to_posts;
+CREATE TABLE tags_to_posts (
+	tag_id INTEGER REFERENCES tags (id),
+	post_id INTEGER REFERENCES posts (id)
 );
 
 INSERT INTO users (name,email,password) VALUES
@@ -30,6 +42,9 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	 "my-first-post.html");
 
+INSERT INTO tags (name) VALUES ("testing");
+INSERT INTO tags_to_posts (tag_id,post_id) VALUES (1,1);
+
 INSERT INTO posts (title,author_id,posted,body,slug) VALUES
 	("My Second Post",1,DATETIME("2013-10-20 12:00:00"),
 "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -38,3 +53,8 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
 
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	 "my-second-post.html");
+
+INSERT INTO tags (name) VALUES ("uncategorized");
+INSERT INTO tags_to_posts (tag_id,post_id) VALUES (1,2);
+INSERT INTO tags_to_posts (tag_id,post_id) VALUES (2,2);
+
