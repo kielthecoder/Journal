821d7cffb6af1e8480bd3f62b62ec3956fbde75e
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2013-11-02 17:52:12 -0400

    Need to work out how to pass new post data to server

--- client/cli.py
+++ client/cli.py
@@ -1,12 +1,45 @@
 import sys
 
+def new_post_from_file(filename):
+	mode = 'body'
+
+	title = ""
+	author = ""
+	tags = list()
+	body = ""
+
+	with open(filename, "rt") as fd:
+		for line in fd:
+			if line[0:4] == '$END':
+				mode = 'end'
+			elif line[0:6] == '$TITLE':
+				mode = 'title'
+				line = line[6:]
+				title = line.strip()
+				print("title = %r" % title)
+			elif line[0:5] == '$TAGS':
+				mode = 'tags'
+				line = line[5:]
+				for tag in line.split():
+					tags.append(tag)
+				print("tags = %r" % tags)
+			else:
+				mode = 'body'
+				body += line
+
+			if mode == 'end':
+				break
+	print("body = %r" % body)
+
+	print("Created new post from %s" % filename)
+
 if __name__ == '__main__':
 	try:
 		if sys.argv[1] == 'new':
 			if len(sys.argv) < 3:
 				print("Usage: %s new <filename>" % sys.argv[0])
 			else:
-				pass
+				new_post_from_file(sys.argv[2])
 	except:
 		print("Too few arguments")
 		print("Usage: %s <command> [args]" % sys.argv[0])
